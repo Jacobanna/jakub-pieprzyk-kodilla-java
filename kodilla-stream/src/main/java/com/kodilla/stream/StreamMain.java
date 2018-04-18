8f7f6e4a5273d43ca9efe864cdd1fb3ec42c7acf
Jacobanna <pieprzyk.jakub@gmail.com>
2018-04-18 21:08:49 +0200

    Stream introduction. Added packages com.kodilla.stream.iterate and com.kodilla.stream.immutable (7.2 example + task)

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -2,6 +2,7 @@ package com.kodilla.stream;
 
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.iterate.NumbersGenerator;
 import com.kodilla.stream.lambda.ExecuteSaySomething;
 import com.kodilla.stream.lambda.Executor;
 import com.kodilla.stream.lambda.ExpressionExecutor;
@@ -10,6 +11,7 @@ import com.kodilla.stream.reference.FunctionalCalculator;
 
 public class StreamMain {
     public static void main(String[] args){
+//        7.1 - LAMBDA EXPRESSION INTRODUCTION
 //        FIRST
 //        Processor processor = new Processor();
 //        ExecuteSaySomething executeSaySomething = new ExecuteSaySomething();
@@ -41,11 +43,14 @@ public class StreamMain {
 //        expressionExecutor.executeExpression(10,5, FunctionalCalculator::divideAByB);
 
 //        TASK 7.1 - POEM BEAUTIFIER
-        PoemBeautifier poemBeautifier = new PoemBeautifier();
-        poemBeautifier.beautify("Ala Ma Kota",(stringToDecorate) -> "AAA" + stringToDecorate + "AAA");
-        poemBeautifier.beautify("Ala Ma Kota", (stringToDecorate) -> stringToDecorate.toUpperCase());
-        poemBeautifier.beautify("Ala Ma Kota", (stringToDecorate) -> stringToDecorate.toLowerCase());
-        poemBeautifier.beautify("Ala Ma Kota", (stringToDecorate) -> stringToDecorate + " " + 173);
+//        PoemBeautifier poemBeautifier = new PoemBeautifier();
+//        poemBeautifier.beautify("Ala Ma Kota",(stringToDecorate) -> "AAA" + stringToDecorate + "AAA");
+//        poemBeautifier.beautify("Ala Ma Kota", (stringToDecorate) -> stringToDecorate.toUpperCase());
+//        poemBeautifier.beautify("Ala Ma Kota", (stringToDecorate) -> stringToDecorate.toLowerCase());
+//        poemBeautifier.beautify("Ala Ma Kota", (stringToDecorate) -> stringToDecorate + " " + 173);
 
+//        7.2 - STREAM INTRODUCTION
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
     }
 }
