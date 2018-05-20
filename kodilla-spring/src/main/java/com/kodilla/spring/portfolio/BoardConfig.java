1eb2daab67379fd990ae4092fdcc90727c1c8497
Jacobanna <pieprzyk.jakub@gmail.com>
2018-05-20 20:57:02 +0200

    configuration of spring in java class task 10.3

--- kodilla-spring/src/main/java/com/kodilla/spring/portfolio/BoardConfig.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/portfolio/BoardConfig.java
@@ -1,27 +1,40 @@
 package com.kodilla.spring.portfolio;
 
+import javafx.concurrent.Task;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class BoardConfig {
-    @Bean
+    @Autowired
+    @Qualifier("toDoList")
+    private TaskList toDoList;
+    @Autowired
+    @Qualifier("inProgressList")
+    private TaskList inProgressList;
+    @Autowired
+    @Qualifier("doneList")
+    private TaskList doneList;
+
+    @Bean(name = "toDoList")
     public TaskList toDoList() {
         return new TaskList();
     }
 
-    @Bean
+    @Bean(name = "inProgressList")
     public TaskList inProgressList() {
         return new TaskList();
     }
 
-    @Bean
+    @Bean(name = "doneList")
     public TaskList doneList() {
         return new TaskList();
     }
 
     @Bean
-    public Board createBoard() {
-        return new Board(toDoList(), inProgressList(), doneList());
+    public Board board() {
+        return new Board(toDoList, inProgressList, doneList);
     }
 }
