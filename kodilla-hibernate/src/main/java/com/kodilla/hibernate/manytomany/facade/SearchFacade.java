f92b110cfcc56b5169bc6cbbfbf94024f47d8017
Jacobanna <pieprzyk.jakub@gmail.com>
2018-08-15 13:19:44 +0200

    Design patterns, Decorator (example 20.2)

---
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/manytomany/facade/SearchFacade.java
@@ -0,0 +1,11 @@
+package com.kodilla.hibernate.manytomany.facade;
+
+import com.kodilla.hibernate.manytomany.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SearchFacade {
+    public
+
+}
